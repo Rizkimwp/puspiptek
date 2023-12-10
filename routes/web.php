d385bf3dc765bb343d81b241bb8d1cb22512ada2
Rizki Maulana <rizki.mwp404@gmail.com>
2023-12-10 21:17:49 +0700

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ShopController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route Login
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/actionLogin', [LoginController::class, 'actionLogin'])->name('actionLogin')->middleware('guest');
+
+// Route Dashboard 
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('/')->middleware('auth', 'role:admin');
+
+
+// route userview 
+
+Route::get('/', [HomeController::class, 'index'])->name('/');
+Route::get('/about', [AboutController::class, 'index'])->name('/about');
+Route::get('/shop', [ShopController::class, 'index'])->name('/shop');
+Route::get('/contact', [ContactController::class, 'index'])->name('/contact');
